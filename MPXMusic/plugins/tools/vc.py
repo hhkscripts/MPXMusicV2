067662a9fb5793b994769b1aa5d25ddaa46aad35
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-30 03:07:29 +0630

    Plugins: vc close adjust

--- MPXMusic/plugins/tools/vc.py
+++ MPXMusic/plugins/tools/vc.py
@@ -1,6 +1,5 @@
 from MPXMusic import app
 from MPXMusic.utils import MPXBin
-from MPXMusic.utils.inline import close_markup
 from MPXMusic.utils.database import get_assistant, get_lang
 from pyrogram import filters
 from pyrogram.enums import ChatType
@@ -50,19 +49,25 @@ async def vc_members(client, message):
             )
 
         # Add close button
-        close_button = [InlineKeyboardButton(text=_["CLOSE_BUTTON"],callback_data=f"close",)],
+        close_button = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close")]])
 
         if len(TEXT) < 4000:
             await msg.edit(
                 TEXT or "⚠️ Nᴏ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛs ɪɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ. 🚫",
-                reply_markup=close_markup(_),
+                reply_markup=close_button,
             )
         else:
             link = await MPXbin(TEXT)
             await msg.edit(
                 f"📄 𝗟𝗶𝗻𝗸 𝗽𝗮𝗿𝗮 𝗹𝗶𝘀𝘁𝗮: {link}",
                 disable_web_page_preview=True,
-                reply_markup=close_markup(_),
+                reply_markup=close_button,
             )
     except ValueError as e:
         await msg.edit("❗ 𝗘𝗿𝗿𝗼𝗿𝗲: 𝗡𝗮̃𝗼 𝗳𝗼𝗶 𝗽𝗼𝘀𝘀𝗶́𝘃𝗲𝗹 𝗰𝗮𝗿𝗿𝗲𝗴𝗮𝗿 𝗮 𝗹𝗶𝘀𝘁𝗮.")
+
+# Callback handler for close button
+@app.on_callback_query(filters.regex("close"))
+async def close_button_handler(client, callback_query):
+    await callback_query.message.delete()
+    await callback_query.answer("Message closed!", show_alert=False)
