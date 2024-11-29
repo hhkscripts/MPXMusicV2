af195464baf75ba4bd4d96815faa1f08a819f06f
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-29 15:20:39 +0630

    Utils: Bot Add Removed.

--- MPXMusic/utils/inline/start.py
+++ MPXMusic/utils/inline/start.py
@@ -19,7 +19,6 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [InlineKeyboardButton(text=_["S_B_10"], url=config.SUPPORT_STORE),],
-        [InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)],        
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
